14cf37094c24f2fc048fb9ec8bbd8f57573c0206
jhantu626 <jhantubala626@gmail.com>
2025-12-31 15:32:17 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const { createServer } = require("node:http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+
+const server = createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+app.use(cors());
+
+io.on("connection", (socket) => {
+  const { user } = socket.handshake.auth;
+  socket.join(user);
+  console.log("user Connected", user, new Date().toLocaleTimeString());
+
+  socket.on("logout",(data)=>{
+    console.log("user logout", user);
+  })
+
+  socket.on("disconnect", () => {
+    console.log("user Disconnected", user);
+    socket.leave(user);
+  });
+});
+
+app.get("/logout", (req, res) => {
+  const { user, status } = req.query;
+  io.to(user).emit("logout", status);
+  res.json({ message: "logout", status: true });
+});
+
+app.post("/logout", (req, res) => {
+  const { roomId, devices } = req.body;
+  io.to(roomId).emit("logout", devices);
+  console.log("user logout", roomId,"devices",devices);
+  res.json({ message: "logout", status: true });
+});
+
+server.listen(4000, "0.0.0.0", () => {
+  console.log("Server running on 4000");
+});
+
